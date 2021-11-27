00461508e3128118f05b2412d7ade182181628a7
priyankagoti <priyanka.bvminfotech@gmail.com>
2021-11-27 17:50:32 +0530

    new commit done.

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 import DashboardLayout from "@/views/Layout/DashboardLayout";
 
@@ -161,7 +161,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   linkActiveClass: "active",
   routes,
 });
