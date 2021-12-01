bb413f81dee065e8a15a005f10de6c2886fcdf2f
priyankagoti <priyanka.bvminfotech@gmail.com>
2021-12-01 15:47:04 +0530

    new commit done.

--- src/components/purchaseOrders/purchaseOrders/_manage.vue
+++ src/components/purchaseOrders/purchaseOrders/_manage.vue
@@ -8,7 +8,7 @@
               <button
                 type="button"
                 class="btn base-button btn-default"
-                @click="update"
+                @click="downloadLabel"
               >
                 Packing Label
               </button>
@@ -30,52 +30,45 @@
                 <label class="form-control-label">{{changeDateFormat(purchaseOrdersData.PODate) }}</label>
               </div>
               <div class="col-md-4">
+                <h4 class="text-dark">Manufacturing SKU Number </h4>
+                <label class="form-control-label">{{purchaseOrdersData.SKUCode }}</label>
+              </div>
+<!--              <div class="col-md-4">
                 <h4 class="text-dark">HSN Code</h4>
                 <label class="form-control-label">{{purchaseOrdersData.HSNCode }}</label>
-              </div>
+              </div>-->
 
               <div class="w-100 mt-4"></div>
 
-              <div class="col-md-4">
+<!--              <div class="col-md-4">
                 <h4 class="text-dark">Item Name</h4>
                 <label class="form-control-label">{{purchaseOrdersData.ItemName }}</label>
+              </div>-->
+              <div class="col-md-4">
+                <h4 class="text-dark">Number of pieces in one Carton</h4>
+                <label class="form-control-label">sdasd</label>
               </div>
               <div class="col-md-4">
                 <h4 class="text-dark">Primary Barcode</h4>
                 <label class="form-control-label">{{purchaseOrdersData.PrimaryBarCode }}</label>
               </div>
               <div class="col-md-4">
-                <h4 class="text-dark">Manufacturing SKU Number </h4>
-                <label class="form-control-label">{{purchaseOrdersData.SKUCode }}</label>
-              </div>
-
-              <div class="w-100 mt-4"></div>
-
-              <div class="col-md-4">
-                <h4 class="text-dark">Purchase Qty</h4>
-                <label class="form-control-label">{{purchaseOrdersData.purchaseQty }}</label>
-              </div>
-              <div class="col-md-4">
-                <h4 class="text-dark">Number of pieces in one Carton</h4>
-                <label class="form-control-label">sdasd</label>
-              </div>
-              <div class="col-md-4">
-                <h4 class="text-dark">MRP per piece</h4>
-                <label class="form-control-label">{{purchaseOrdersData.MRP }}</label>
+                <h4 class="text-dark">Secondary Barcode </h4>
+                <label class="form-control-label">{{purchaseOrdersData.secondaryBarcode }}</label>
               </div>
 
               <div class="w-100 mt-4"></div>
 
               <div class="col-md-4">
                 <h4 class="text-dark">Buyer Margin</h4>
-                <label class="form-control-label">dsfd</label>
+                <label class="form-control-label">{{ purchaseOrdersData.BuyerMarginPer }}</label>
               </div>
               <div class="col-md-4">
                 <h4 class="text-dark">Buyer Margin Amt</h4>
                 <label class="form-control-label">{{purchaseOrdersData.buyerMarginAmt }}</label>
               </div>
               <div class="col-md-4">
-                <h4 class="text-dark">Biilling Price</h4>
+                <h4 class="text-dark">Billing Price</h4>
                 <label class="form-control-label">{{purchaseOrdersData.billingPrice }}</label>
               </div>
 
@@ -86,47 +79,108 @@
                 <label class="form-control-label">{{purchaseOrdersData.taxRate }}</label>
               </div>
               <div class="col-md-4">
-                <h4 class="text-dark">SGST Amt - 2.5%</h4>
-                <label class="form-control-label">{{purchaseOrdersData.SGSTAmt }}</label>
+                <h4 class="text-dark">Delivery Location </h4>
+                <label class="form-control-label">{{purchaseOrdersData.deliveryLocation }}</label>
               </div>
-              <div class="col-md-4">
-                <h4 class="text-dark">CGST Amt - 2.5%</h4>
-                <label class="form-control-label">{{purchaseOrdersData.CGSTAmt }}</label>
-              </div>
-
-              <div class="w-100 mt-4"></div>
-
-              <div class="col-md-4">
-                <h4 class="text-dark">IGST Amt - 0% </h4>
-                <label class="form-control-label">{{purchaseOrdersData.IGSTAmt }}</label>
-              </div>
-              <div class="col-md-4">
-                <h4 class="text-dark">Total Price</h4>
-                <label class="form-control-label">{{purchaseOrdersData.totalPrice }}</label>
-              </div>
-              <div class="col-md-4">
-                <h4 class="text-dark">Secondary Barcode </h4>
-                <label class="form-control-label">{{purchaseOrdersData.secondaryBarcode }}</label>
-              </div>
-
-              <div class="w-100 mt-4"></div>
-
-
               <div class="col-md-4">
                 <h4 class="text-dark">Document Upload (Invoice/Packing Label/etc.)</h4>
                 <label class="form-control-label">{{purchaseOrdersData.documentUpload }}</label>
               </div>
-              <div class="col-md-4">
-                <h4 class="text-dark">Delivery Location </h4>
-                <label class="form-control-label">{{purchaseOrdersData.deliveryLocation }}</label>
-              </div>
-              <div class="w-100"></div>
             </div>
-
-
           </div>
         </div>
       </form>
+    </div>
+    <div class="mt-4">
+      <Table>
+        <template #thead>
+          <tr>
+            <th>Item code</th>
+            <th>Item Name</th>
+            <th>HSN Code</th>
+            <th>Quantity</th>
+            <th>Selling Price </th>
+            <th>Price</th>
+            <th>Total Cost</th>
+            <th>GST Code</th>
+            <th>GST %</th>
+            <th>Taxable Amount</th>
+            <th>Tax Amount</th>
+            <th>Disc %</th>
+            <th>Disc Amount</th>
+            <th>CGST %</th>
+            <th>CGST Amount</th>
+            <th>SGST/UGST %</th>
+            <th>SGST/UGST Amount</th>
+            <th>IGST %</th>
+            <th>IGST Amount</th>
+            <th>GST Cess</th>
+            <th>GST Cess Amount</th>
+            <th>Total Amount</th>
+          </tr>
+        </template>
+        <template #tbody>
+          <tr v-for="item in itemList" :key="item.itemCode">
+            <td>{{item.itemCode}}</td>
+            <td>{{item.itemName}}</td>
+            <td>{{item.HSNCode}}</td>
+            <td>{{item.qty}}</td>
+            <td>{{item.sellingPrice}}</td>
+            <td>{{item.price}}</td>
+            <td>{{item.totalCost}}</td>
+            <td>{{item.GSTCode}}</td>
+            <td>{{item.GSTInPer}}</td>
+            <td>{{item.taxableAMT}}</td>
+            <td>{{item.taxAMT}}</td>
+            <td>{{item.discInPer}}</td>
+            <td>{{item.discAMt}}</td>
+            <td>{{item.CGSTInPer}}</td>
+            <td>{{item.CGSTAMT}}</td>
+            <td>{{item.SGSTInPer}}</td>
+            <td>{{item.SGSTAMT}}</td>
+            <td>{{item.IGSTInPer}}</td>
+            <td>{{item.IGSTAMT}}</td>
+            <td>{{item.GSTCess}}</td>
+            <td>{{item.GstCessAmount}}</td>
+            <td>{{item.taxAMT}}</td>
+
+          </tr>
+        </template>
+      </Table>
+    </div>
+    <div v-show="invisibleTable">
+      <table id="myTable">
+        <thead>
+        <tr>
+          <th>PO Number</th>
+          <th>PO Date</th>
+          <th>HSN Code</th>
+          <th>Item Name</th>
+          <th>Primary Barcode</th>
+        </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{{purchaseOrdersData.PONumber}}</td>
+            <td>{{changeDateFormat(purchaseOrdersData.PODate) }}</td>
+            <td>{{purchaseOrdersData.HSNCode }}</td>
+            <td>{{purchaseOrdersData.ItemName }}</td>
+            <td>{{purchaseOrdersData.PrimaryBarCode }}</td>
+          </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>{{purchaseOrdersData.ItemName }}</td>
+        </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>{{purchaseOrdersData.ItemName }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </card>
 </template>
@@ -135,16 +189,19 @@
 
 import axios from "axios";
 import _ from "lodash";
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 export default {
   components: {},
   props: ["id"],
   data() {
     return {
+      invisibleTable:false,
       imageURL: null,
       purchaseOrdersData:[],
+      itemList:[],
       status: "",
-
     };
   },
   mounted(){
@@ -157,8 +214,10 @@ export default {
       axios
         .get(`api/purChaseOrder/get/${this.id}`)
         .then((response) => {
-          console.log(response.data[0]);
           this.purchaseOrdersData = _.merge(this.purchaseOrdersData, response.data[0]);
+          this.itemList=this.purchaseOrdersData.itemList;
+         // this.labelData= _.map(response.data[0], function square(n) {return n;});
+          console.log( this.purchaseOrdersData.itemList);
 
         })
         .catch((error) => {
@@ -166,6 +225,11 @@ export default {
         });
     },
 
+    downloadLabel(){
+      const doc = new jsPDF();
+      doc.autoTable({ html: '#myTable' });
+      doc.save('document.pdf');
+    },
     update() {
       axios
         .put(`api/product/edit/${this.productDetails._id}`, this.productDetails)
--- src/views/masterProducts/products/index.vue
+++ src/views/masterProducts/products/index.vue
@@ -7,6 +7,52 @@
             <h3 class="mb-0">Products</h3>
           </div>
           <div class="col-lg-8 text-right">
+            <base-dropdown
+              menu-on-right
+              class="nav-item"
+              tag="li"
+              title-tag="a"
+              title-classes="nav-link pr-0"
+            >
+              <template v-slot:title-container>
+                <a href="#" class="nav-link pr-0" @click.prevent>
+                  <div class="media align-items-center">
+                    <div class="media-body ml-2 d-none d-lg-block">
+                      <button class="mb-0 rounded-circle border border-light btn-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                        <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                      </svg>
+                      </button>
+                    </div>
+                  </div>
+                </a>
+              </template>
+                <router-link :to="{ name: 'ProductCreate' }" >
+                  <button type="button" class="dropdown-item">Add Product</button>
+                </router-link>
+
+                <button type="button" class="dropdown-item" @click="onExportApi">
+                  Template
+                </button>
+
+                <button type="button" class="dropdown-item">
+                  <export-excel :data="productData"> Download </export-excel>
+                </button>
+
+                <button
+                  type="button"
+                  class="dropdown-item"
+                  @click="visibleCard = true"
+                  data-toggle="modal"
+                  data-target="#myModal"
+                >
+                  Upload
+                </button>
+
+            </base-dropdown>
+          </div>
+
+<!--          <div class="col-lg-8 text-right">
             <button type="button" class="btn base-button btn-default" @click="onExportApi">
                Sample Excel
             </button>
@@ -26,7 +72,7 @@
             <router-link :to="{ name: 'ProductCreate' }">
               <base-button type="default">Add Product</base-button>
             </router-link>
-          </div>
+          </div>-->
           <div v-if="visibleCard">
             <DataModal :title="'Upload Excel'">
               <template v-slot:body>
@@ -96,6 +142,9 @@ export default {
     fetchProduct() {
       axios.get(`/api/product/get`).then((response) => {
         this.productData = response.data;
+        console.log(response.data[0]);
+        this.response= _.map(response.data[0], function square(n) {return n;});
+        console.log(this.response);
       });
     },
     uploadExcel() {
