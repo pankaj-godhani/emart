11a862a467f6b78b118f0b5d470fe7ab08c10ceb
priyankagoti <priyanka.bvminfotech@gmail.com>
2022-01-08 17:58:06 +0530

    new commit done.

---
+++ src/views/auth/Logout.vue
@@ -0,0 +1,27 @@
+<template>
+
+</template>
+
+<script>
+    import store from '../../state/store';
+
+    export default {
+        beforeRouteEnter (to, from, next) {
+            store.dispatch('auth/logOut');
+
+            next({name: 'Login'});
+        },
+
+        data() {
+            return {
+
+            }
+        },
+
+        mounted() {
+
+        },
+
+        methods: {}
+    }
+</script>
